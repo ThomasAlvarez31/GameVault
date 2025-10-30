0eb0f74a78ad1aec1293fe0c0ada3d2dd90607a9
Thomas31 <163472229+ThomasAlvarez31@users.noreply.github.com>
2025-10-30 10:32:03 -0300

    Delete app directory

--- app/src/main/java/com/example/gamevault/viewmodel/AnimePost.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gamevault.viewmodel
-
-data class AnimePost(
-    val id: Int,
-    val titulo: String,
-    val descripcion: String,
-    val imageResId: Int // ID del recurso drawable
-)
